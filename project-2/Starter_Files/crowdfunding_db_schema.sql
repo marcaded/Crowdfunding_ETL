5d203d4df64c33026ad342e6d6c83495816f6179
marcaded <134164799+marcaded@users.noreply.github.com>
2023-09-07 19:07:35 -0400

    Add files via upload

---
+++ project-2/Starter_Files/crowdfunding_db_schema.sql
@@ -0,0 +1,61 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/M9Z1DE
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- conceptual
+
+CREATE TABLE "Contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" str   NOT NULL,
+    "last_name" str   NOT NULL,
+    "email" str   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "contact_id" int   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "company_name" str   NOT NULL,
+    "description" str   NOT NULL,
+    "goal" int   NOT NULL,
+    "pledged" int   NOT NULL,
+    "outcome" str   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" str   NOT NULL,
+    "currency" str   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" int   NOT NULL,
+    "subcategory_id" int   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "Category" (
+    "category_id" int   NOT NULL,
+    "category" str   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" int   NOT NULL,
+    "subcategory" str   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
